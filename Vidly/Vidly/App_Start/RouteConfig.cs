039231091f30059a3f640f7b366033bc04c21f0a
tourangeaus <enmass90210@gmail.com>
2017-02-21 16:22:56 -0700

    Routing & Constraints

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -12,6 +13,17 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+            //    name: "MoviesByReleaseDate",
+            //    url: "movie/released/{year}/{month}",
+            //    defaults: new { controller = "Movie", action= "ByReleaseDate"},
+            //    constraints: new { year = @"\d{4}", month = @"\d{2}" }
+            //    // Specific Range Constraint
+            //    // constraints: new { year = @"2015|2016", month = @"\d{2}" }
+            //);
 
             routes.MapRoute(
                 name: "Default",
--- Vidly/Vidly/Controllers/MovieController.cs
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -21,5 +21,27 @@ namespace Vidly.Controllers
             // return new EmptyResult();
             // return RedirectToAction("Index", "Home", new { page = 1, sortBy ="name"});
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movie/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
